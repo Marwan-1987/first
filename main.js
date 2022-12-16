d0b50a76ca986ae690b9474a614fb2bdec6a0a19
Marwan-1987 <marwan.87510@gmail.com>
2022-12-16 13:17:10 -0800

    updet

---
+++ main.js
@@ -0,0 +1,69 @@
+console.log("marwan")
+
+
+
+
+
+document.getElementById("world cup").innerHTML="brazil";
+
+
+
+
+document.getElementById("libya").innerHTML="Qatar 2022 becomes first FIFA World Cup™ to achieve international sustainability ";
+
+
+
+
+
+var one=500;
+var two=200;
+var three=100;
+var four=300;
+var five=800;
+var six=600;
+var seven=500;
+var eight=200;
+var nine=300;
+var ten=300;
+
+
+var total=(one+two+three+four+five+six+seven+eight+nine+ten)/10
+
+
+document.getElementById("totl").innerHTML=total;
+
+
+
+
+var $name="shawki";
+console.log($name);
+
+
+
+var age=35;
+console.log(age);
+
+
+var m=5000;
+var t=2000;
+console.log(m==t)
+
+
+var g=7000;
+var n=7000;
+console.log(g==n)
+
+
+
+var y=3000;
+var b=("3000")
+console.log(y===b)
+
+
+
+
+
+
+
+
+
